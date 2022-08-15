0923987522c5cfb0a573ec0d1e4b3e24fdd025b5
Sing <gd81818888@gmail.com>
2022-08-15 16:41:34 +0800

    登录内容-基本结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Card } from 'antd'
+import './index.css'
+import logo from '../../assets/logo.png'
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        <Button type='primary'>按钮</Button>
+      <div className='login'>
+        <Card className='login-container'>
+            <img src={logo} className='login-logo'/>
+
+        </Card>
       </div>
     )
   }
